b02c1b103fc318f61b69b6c2712f1eca1e899873
nealmick <nealmick99@gmail.com>
2025-03-10 11:45:24 -0400

    refactor

--- editor/editor.h
+++ editor/editor.h
@@ -35,12 +35,6 @@ class Editor
     int getLineFromPos(const std::vector<int> &line_starts, int pos);
     float calculateTextWidth(const std::string &text, const std::vector<int> &line_starts);
 
-    // Clipboard operations (could potentially be moved to EditorCopyPaste)
-    void copySelectedText(const std::string &text, const EditorState &state);
-    void cutSelectedText(std::string &text, std::vector<ImVec4> &colors, EditorState &state, bool &text_changed);
-    void cutWholeLine(std::string &text, std::vector<ImVec4> &colors, EditorState &state, bool &text_changed);
-    void pasteText(std::string &text, std::vector<ImVec4> &colors, EditorState &state, bool &text_changed);
-
   private:
     // Main render pipeline
     bool validateAndResizeColors(std::string &text, std::vector<ImVec4> &colors);
@@ -48,12 +42,5 @@ class Editor
     ImVec2 renderLineNumbersPanel(float line_number_width, float editor_top_margin);
     void beginTextEditorChild(const char *label, float remaining_width, float content_width, float content_height, float &current_scroll_y, float &current_scroll_x, ImVec2 &text_pos, float editor_top_margin, float text_left_margin, EditorState &editor_state);
     void renderEditorContent(const std::string &text, const std::vector<ImVec4> &colors, EditorState &editor_state, float line_height, const ImVec2 &text_pos);
-    void renderTextWithSelection(ImDrawList *drawList, const ImVec2 &pos, const std::string &text, const std::vector<ImVec4> &colors, const EditorState &state, float line_height);
-
-    // Input handling
-    void handleEditorInput(std::string &text, EditorState &state, const ImVec2 &text_start_pos, float line_height, bool &text_changed, std::vector<ImVec4> &colors, CursorVisibility &ensure_cursor_visible);
-    void processTextEditorInput(std::string &text, EditorState &editor_state, const ImVec2 &text_start_pos, float line_height, bool &text_changed, std::vector<ImVec4> &colors, CursorVisibility &ensure_cursor_visible, int initial_cursor_pos);
-
-    // Editor commands
-    void processUndoRedo(std::string &text, std::vector<ImVec4> &colors, EditorState &state, bool &text_changed, CursorVisibility &ensure_cursor_visible, bool shift_pressed);
+    void renderText(ImDrawList *drawList, const ImVec2 &pos, const std::string &text, const std::vector<ImVec4> &colors, const EditorState &state, float line_height);
 };
--- editor/editor_keyboard.h
+++ editor/editor_keyboard.h
@@ -15,15 +15,23 @@ class EditorKeyboard
     EditorKeyboard();
     ~EditorKeyboard() = default;
 
-    // Text input handling
     void handleCharacterInput(std::string &text, std::vector<ImVec4> &colors, EditorState &state, bool &text_changed, int &input_start, int &input_end);
     void handleEnterKey(std::string &text, std::vector<ImVec4> &colors, EditorState &state, bool &text_changed, int &input_end);
     void handleDeleteKey(std::string &text, std::vector<ImVec4> &colors, EditorState &state, bool &text_changed, int &input_end);
     void handleBackspaceKey(std::string &text, std::vector<ImVec4> &colors, EditorState &state, bool &text_changed, int &input_start);
-
     void handleTextInput(std::string &text, std::vector<ImVec4> &colors, EditorState &state, bool &text_changed);
+
     void processFontSizeAdjustment(CursorVisibility &ensure_cursor_visible);
     void processSelectAll(std::string &text, EditorState &state, CursorVisibility &ensure_cursor_visible);
+
+    void processTextEditorInput(std::string &text, EditorState &editor_state, const ImVec2 &text_start_pos, float line_height, bool &text_changed, std::vector<ImVec4> &colors, CursorVisibility &ensure_cursor_visible, int initial_cursor_pos);
+    void handleEditorKeyboardInput(std::string &text, EditorState &state, const ImVec2 &text_start_pos, float line_height, bool &text_changed, std::vector<ImVec4> &colors, CursorVisibility &ensure_cursor_visible);
+
+    void handleArrowKeyVisibility(CursorVisibility &ensure_cursor_visible);
+
+    void updateCursorVisibilityOnTextChange(bool text_changed, CursorVisibility &ensure_cursor_visible);
+
+    void processUndoRedo(std::string &text, std::vector<ImVec4> &colors, EditorState &state, bool &text_changed, CursorVisibility &ensure_cursor_visible, bool shift_pressed);
 };
 
 // Global instance
