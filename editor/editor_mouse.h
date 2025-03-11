fe5436a08f7bd37e8e0dd7da0b45bf1886e78598
nealmick <nealmick99@gmail.com>
2025-03-11 15:56:38 -0400

    added right click menu

--- editor/editor_mouse.h
+++ editor/editor_mouse.h
@@ -20,10 +20,15 @@ class EditorMouse
     // Character position calculation from mouse coordinates
     int getCharIndexFromCoords(const std::string &text, const ImVec2 &click_pos, const ImVec2 &text_start_pos, const std::vector<int> &line_starts, float line_height);
 
+    // Context menu handler
+    void handleContextMenu(std::string &text, std::vector<ImVec4> &colors, EditorState &state, bool &text_changed);
+
   private:
     // Selection state
     bool is_dragging;
     int anchor_pos;
+    bool show_context_menu;
+    ImVec2 context_menu_pos;
 
     // Handle different mouse actions
     void handleMouseClick(EditorState &state, int char_index, const std::vector<int> &line_starts);
