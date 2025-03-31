c6558ff16d17fa2e91ce032d71d8964a72d402f9
nealmick <nealmick99@gmail.com>
2025-03-30 23:26:23 -0400

    added go to ref

---
+++ lsp/lsp_goto_ref.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "../lib/json.hpp"
+#include "editor_types.h"
+#include "imgui.h"
+#include "lsp_manager.h"
+#include <memory>
+#include <string>
+#include <vector>
+
+struct ReferenceLocation
+{
+    std::string uri;
+    int startLine;
+    int startChar;
+    int endLine;
+    int endChar;
+};
+
+using json = nlohmann::json;
+
+class LSPGotoRef
+{
+  public:
+    LSPGotoRef();
+    ~LSPGotoRef();
+
+    // Core find references functionality
+    bool findReferences(const std::string &filePath, int line, int character);
+
+    // Reference options window rendering
+    void renderReferenceOptions();
+    bool hasReferenceOptions() const;
+
+  private:
+    // Helper methods
+    void parseReferenceResponse(const std::string &response);
+    int getNextRequestId() { return ++currentRequestId; }
+
+    // Request tracking (using a different range than LSPGotoDef)
+    int currentRequestId = 3000;
+
+    // Reference options state
+    std::vector<ReferenceLocation> referenceLocations;
+    int selectedReferenceIndex = 0;
+    bool showReferenceOptions = false;
+};
+
+// Global instance
+extern LSPGotoRef gLSPGotoRef;
