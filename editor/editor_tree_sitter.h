984665ad277a3fc1ba178e1c3a26a864045254fd
nealmick <nealmick99@gmail.com>
2025-04-15 13:42:34 -0400

    implement tree-sitter

---
+++ editor/editor_tree_sitter.h
@@ -0,0 +1,38 @@
+// editor_tree_sitter.h
+#pragma once
+#include "../util/settings.h"
+#include "imgui.h"
+#include <iostream>
+#include <string>
+#include <tree_sitter/api.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+struct ThemeColors
+{
+    ImVec4 keyword;
+    ImVec4 string;
+    ImVec4 number;
+    ImVec4 comment;
+    ImVec4 text;
+    ImVec4 function;
+};
+
+class TreeSitter
+{
+  public:
+    static void parseAndPrintAST(const std::string &fileContent, std::vector<ImVec4> &fileColors);
+    static void updateThemeColors();
+    static void printColors();
+
+    static bool colorsNeedUpdate;    // settings theme changed
+    static ThemeColors cachedColors; // cached settings theme colors, so we dont read from disk
+
+    static const ImVec4 &convertNodeType(const std::string &nodeType);
+
+    static void setColors(const std::string &fileContent, std::vector<ImVec4> &fileColors, int start, int end, const ImVec4 &color);
+
+  private:
+    static void traverseAndPrint(const std::string &fileContent, std::vector<ImVec4> &fileColors, TSNode node, int depth, bool is_last, const std::vector<bool> &hierarchy);
+};
