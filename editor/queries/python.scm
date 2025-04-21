85ab7fdeeaa8e315fcbf8011b977a632d8bd8ea6
nealmick <nealmick99@gmail.com>
2025-04-21 10:27:42 -0400

    adding more languages

--- editor/queries/python.scm
+++ editor/queries/python.scm
@@ -1,137 +1,65 @@
-; Identifier naming conventions
+; ==============================================================================
+; Python Highlighting Query
+; ==============================================================================
 
-(identifier) @variable
-
-((identifier) @constructor
- (#match? @constructor "^[A-Z]"))
-
-((identifier) @constant
- (#match? @constant "^[A-Z][A-Z_]*$"))
-
-; Function calls
-
-(decorator) @function
-(decorator
-  (identifier) @function)
-
-(call
-  function: (attribute attribute: (identifier) @function.method))
-(call
-  function: (identifier) @function)
-
-; Builtin functions
-
-((call
-  function: (identifier) @function.builtin)
- (#match?
-   @function.builtin
-   "^(abs|all|any|ascii|bin|bool|breakpoint|bytearray|bytes|callable|chr|classmethod|compile|complex|delattr|dict|dir|divmod|enumerate|eval|exec|filter|float|format|frozenset|getattr|globals|hasattr|hash|help|hex|id|input|int|isinstance|issubclass|iter|len|list|locals|map|max|memoryview|min|next|object|oct|open|ord|pow|print|property|range|repr|reversed|round|set|setattr|slice|sorted|staticmethod|str|sum|super|tuple|type|vars|zip|__import__)$"))
-
-; Function definitions
-
+; --- Function Definitions ---
 (function_definition
   name: (identifier) @function)
 
-(attribute attribute: (identifier) @property)
-(type (identifier) @type)
+; --- Class Definitions ---
+(class_definition
+  name: (identifier) @type)
 
-; Literals
+; --- Function/Method Calls ---
+(call
+  function: (identifier) @function)
 
-[
-  (none)
-  (true)
-  (false)
-] @constant.builtin
+(call
+  function: (attribute
+    attribute: (identifier) @method))
 
-[
-  (integer)
-  (float)
-] @number
+; --- Built-in Exceptions/Classes ---
+((identifier) @type.builtin
+ (#match? @type.builtin "^[A-Z][A-Za-z0-9_]*$")
+ (#is-not? @type.builtin "self|True|False|None"))
 
+; --- Literals & Keywords ---
 (comment) @comment
 (string) @string
-(escape_sequence) @escape
-
-(interpolation
-  "{" @punctuation.special
-  "}" @punctuation.special) @embedded
+(integer) @number
+(float) @number
+(true) @boolean
+(false) @boolean
+(none) @constant.builtin
 
 [
-  "-"
-  "-="
-  "!="
-  "*"
-  "**"
-  "**="
-  "*="
-  "/"
-  "//"
-  "//="
-  "/="
-  "&"
-  "&="
-  "%"
-  "%="
-  "^"
-  "^="
-  "+"
-  "->"
-  "+="
-  "<"
-  "<<"
-  "<<="
-  "<="
-  "<>"
-  "="
-  ":="
-  "=="
-  ">"
-  ">="
-  ">>"
-  ">>="
-  "|"
-  "|="
-  "~"
-  "@="
-  "and"
-  "in"
-  "is"
-  "not"
-  "or"
-  "is not"
-  "not in"
+  "def" "class" "return" "if" "else" "elif" "for" "while"
+  "import" "from" "as" "try" "except" "finally" "pass"
+  "continue" "break" "raise" "with" "lambda" "nonlocal" "global"
+] @keyword
+
+[
+  "and" "or" "not" "in" "is"
+] @keyword.operator
+
+[
+  "=" "+" "-" "*" "/" "%" "**" "//" "==" "!=" "<" ">" "<=" ">="
+  ":=" "+=" "-=" "*=" "/=" "%=" "**=" "//=" "&" "|" "^" "~" "<<" ">>"
+  "@" "->"
 ] @operator
 
-[
-  "as"
-  "assert"
-  "async"
-  "await"
-  "break"
-  "class"
-  "continue"
-  "def"
-  "del"
-  "elif"
-  "else"
-  "except"
-  "exec"
-  "finally"
-  "for"
-  "from"
-  "global"
-  "if"
-  "import"
-  "lambda"
-  "nonlocal"
-  "pass"
-  "print"
-  "raise"
-  "return"
-  "try"
-  "while"
-  "with"
-  "yield"
-  "match"
-  "case"
-] @keyword
+; --- Special Variables ---
+((identifier) @variable.builtin
+ (#eq? @variable.builtin "self"))
+
+; --- Decorators ---
+(decorator
+  "@" @punctuation.special
+  (identifier) @function)
+
+; --- Type Annotations ---
+(type (identifier) @type)
+
+; --- Exceptions ---
+(except_clause
+  (identifier) @type)
