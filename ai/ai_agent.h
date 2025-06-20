2d6be1f211d6b0080e2ac133ed68cf7c02502ba2
nealmick <nealmick99@gmail.com>
2025-06-20 12:20:12 -0400

    update text box content while focused

--- ai/ai_agent.h
+++ ai/ai_agent.h
@@ -1,8 +1,8 @@
+// ai_agent.h
 #pragma once
 #include <imgui.h>
 #include <vector>
 #include <string>
-#include "native_text_input.h"
 
 class AIAgent {
 public:
@@ -12,27 +12,15 @@ public:
     };
 
     AIAgent();
-    ~AIAgent();
-    
     void render(float agentPaneWidth);
     void sendMessage(const char* msg);
     void AgentInput(const ImVec2& textBoxSize, float textBoxWidth, float horizontalPadding);
     void printAllMessages() const;
 
-    // Expose nativeInput for focus checking
-    NativeTextInput* getNativeInput() const { return nativeInput; }
-
 private:
     std::vector<Message> messages;
     char inputBuffer[256] = {0};
+    unsigned int frameCounter;
     void renderMessageHistory(const ImVec2& size);
     bool scrollToBottom = false;
-    
-    // Native text input widget
-    NativeTextInput* nativeInput;
-    std::string currentInputText;
-    
-    // Callback handlers
-    void onTextChanged(const std::string& text);
-    void onEnterPressed();
 };
