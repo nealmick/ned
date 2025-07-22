248bdd20a594075731c3db36570b1bc71fc3df67
nealmick <nealmick99@gmail.com>
2025-07-22 15:53:06 -0400

    .

--- ned.cpp
+++ ned.cpp
@@ -2,6 +2,7 @@
 File: ned.cpp
 Description: Main application class implementation for NED text editor.
 */
+
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 #include "imgui_internal.h"
