075f81574da65b210bf8dab9bc3f8ea570f9ef02
nealmick <nealmick@github.com>
2025-08-15 14:09:56 -0400

    fixes for opening file with newline chars and fixes for delete backspace on windows

--- editor/editor_keyboard.cpp
+++ editor/editor_keyboard.cpp
@@ -107,6 +107,15 @@ void EditorKeyboard::handleBackspaceKey()
 				if (prev != editor_state.fileContent.begin())
 				{
 					utf8::unchecked::prior(prev);
+					
+#ifdef PLATFORM_WINDOWS
+					// Special handling for Windows line endings (\r\n)
+					// If we're about to delete \n and the previous char is \r, delete both
+					if (*prev == '\n' && prev != editor_state.fileContent.begin() && *(prev - 1) == '\r')
+					{
+						--prev; // Move back to include the \r as well
+					}
+#endif
 				}
 				int start_pos = str_index_at(editor_state.fileContent, prev);
 				ranges_to_delete.emplace_back(start_pos, pos);
