03626124840716999cd9344d241f3ca6911d51a0
nealmick <nealmick@github.com>
2025-08-15 16:16:21 -0400

    windows action build

--- build-win.bat
+++ build-win.bat
@@ -16,9 +16,29 @@ if not exist build (
 
 cd build
 
-REM Configure with CMake using vcpkg toolchain
+REM Try to find vcpkg toolchain file
+set VCPKG_TOOLCHAIN_FILE=
+if exist "%CD%\vcpkg\scripts\buildsystems\vcpkg.cmake" (
+    set VCPKG_TOOLCHAIN_FILE=%CD%\vcpkg\scripts\buildsystems\vcpkg.cmake
+) else if exist "%CD%\..\vcpkg\scripts\buildsystems\vcpkg.cmake" (
+    set VCPKG_TOOLCHAIN_FILE=%CD%\..\vcpkg\scripts\buildsystems\vcpkg.cmake
+) else if defined VCPKG_ROOT (
+    set VCPKG_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake
+) else (
+    echo Warning: vcpkg toolchain file not found!
+    echo Please set VCPKG_ROOT environment variable or ensure vcpkg is in the project directory
+    echo Attempting build without vcpkg...
+)
+
+REM Configure with CMake using vcpkg toolchain if available
 echo Configuring with CMake...
-cmake .. -G "Visual Studio 17 2022" -A x64 -DCMAKE_TOOLCHAIN_FILE="C:/Users/nealm/source/vcpkg/scripts/buildsystems/vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows
+if defined VCPKG_TOOLCHAIN_FILE (
+    echo Using vcpkg toolchain: %VCPKG_TOOLCHAIN_FILE%
+    cmake .. -G "Visual Studio 17 2022" -A x64 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_TOOLCHAIN_FILE%" -DVCPKG_TARGET_TRIPLET=x64-windows
+) else (
+    echo Building without vcpkg toolchain
+    cmake .. -G "Visual Studio 17 2022" -A x64
+)
 
 REM Check if configuration succeeded
 if %errorlevel% neq 0 (
