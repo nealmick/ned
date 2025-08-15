05acb48194314dccb7fd23bc070dd8e4754ed439
nealmick <nealmick@github.com>
2025-08-15 12:57:37 -0400

    Add vcpkg_installed/ to .gitignore
    The vcpkg_installed directory contains generated packages from vcpkg.json
    and should not be committed to the repository. Each developer creates
    their own local copy via 'vcpkg install'.

--- vcpkg_installed/vcpkg/blds/zlib/x64-windows-dbg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/nealm/source/ned/vcpkg_installed/vcpkg/blds/zlib/src/v1.3.1-2e5db616bf.clean
-# Build directory: C:/Users/nealm/source/ned/vcpkg_installed/vcpkg/blds/zlib/x64-windows-dbg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
